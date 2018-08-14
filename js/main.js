817aecad4a74e06e20a4133d634306aef3e49736
Ben <ben399399@naver.com>
2018-08-14 16:44:22 +0900

    Finished quote copy to clipboard

--- js/main.js
+++ js/main.js
@@ -23,14 +23,14 @@ var leftpaneAutoOpacity = ()=>{
 };
 
 var nameAutoSize = ()=>{
-    if(window.innerWidth<720){
+    if(window.innerWidth<1024){
         var bodyrect = document.getElementsByClassName("item-card")[0].getBoundingClientRect();
         leftpane.style.opacity = vh(70)/window.innerHeight;
         fontsize = 140;
         lineheight = 110;
     }
     else{
-        fontsize=lineheight= Math.min(vw(9),vh(2)*vh(2)*0.4);
+        fontsize=lineheight= Math.min(vw(9),vh(2)*vh(2)*0.4)*0.7;
         leftpane.style.opacity = 1;
     }
     nom.fontSize=fontsize+'px';
@@ -146,14 +146,6 @@ for (var i=0;i<ProjectData.length;i++){
     langs.innerHTML="";
 };
 
-class Quote{
-    constructor(MLA,APA,ISO690){
-        this.MLA=MLA;
-        this.APA=APA;
-        this.ISO690=ISO690;
-    }
-}
-
 class MyPaper{
     constructor(doi,name,author,journal,quote){
         this.doi=doi;
@@ -180,11 +172,11 @@ var PaperData=[
             name="Parallel cloth simulation with effective collision detection for interactive AR application", 
             author="Minsang Kim, Nak-Jun Sung, Sang-Joon Kim, Yoo-Joo Choi, Min Hong", 
             journal="Multimedia Tools and Applications , 2018. 05.(SCIE)", 
-            quote=new Quote(
-                AMA="Kim, M., Sung, N. J., Kim, S. J., Choi, Y. J., & Hong, M. (2018). Parallel cloth simulation with effective collision detection for interactive AR application. Multimedia Tools and Applications, 1-18.",
-                MLA='Kim, Minsang, et al. "Parallel cloth simulation with effective collision detection for interactive AR application." Multimedia Tools and Applications (2018): 1-18.',
-                ISO690="KIM, Minsang, et al. Parallel cloth simulation with effective collision detection for interactive AR application. Multimedia Tools and Applications, 2018, 1-18."
-            )
+            quote={
+                AMA:"Kim, M., Sung, N. J., Kim, S. J., Choi, Y. J., & Hong, M. (2018). Parallel cloth simulation with effective collision detection for interactive AR application. Multimedia Tools and Applications, 1-18.",
+                MLA:'Kim, Minsang, et al. "Parallel cloth simulation with effective collision detection for interactive AR application." Multimedia Tools and Applications (2018): 1-18.',
+                ISO690:"KIM, Minsang, et al. Parallel cloth simulation with effective collision detection for interactive AR application. Multimedia Tools and Applications, 2018, 1-18."
+            }
         ), 
         
     ], 
@@ -194,11 +186,11 @@ var PaperData=[
             name="OpenGL과 Unity간의 GPU를 이용한 Particle Simulation의 성능 비교",
             author="김민상, 성낙준, 최유주, 홍민", 
             journal="정보처리학회논문지. 소프트웨어 및 데이터 공학, 6권 10호", 
-            quote=new Quote(
-                AMA="김민상, 성낙준, 최유주, & 홍민. (2017). OpenGL 과 Unity 간의 GPU 를 이용한 Particle Simulation 의 성능 비교. 정보처리학회논문지. 소프트웨어 및 데이터 공학, 6(10), 479-486.",
-                MLA='김민상, et al. "OpenGL 과 Unity 간의 GPU 를 이용한 Particle Simulation 의 성능 비교." 정보처리학회논문지. 소프트웨어 및 데이터 공학 6.10 (2017): 479-486.',
-                ISO690="김민상, et al. OpenGL 과 Unity 간의 GPU 를 이용한 Particle Simulation 의 성능 비교. 정보처리학회논문지. 소프트웨어 및 데이터 공학, 2017, 6.10: 479-486."
-            )
+            quote={
+                AMA:"김민상, 성낙준, 최유주, & 홍민. (2017). OpenGL 과 Unity 간의 GPU 를 이용한 Particle Simulation 의 성능 비교. 정보처리학회논문지. 소프트웨어 및 데이터 공학, 6(10), 479-486.",
+                MLA:'김민상, et al. "OpenGL 과 Unity 간의 GPU 를 이용한 Particle Simulation 의 성능 비교." 정보처리학회논문지. 소프트웨어 및 데이터 공학 6.10 (2017): 479-486.',
+                ISO690:"김민상, et al. OpenGL 과 Unity 간의 GPU 를 이용한 Particle Simulation 의 성능 비교. 정보처리학회논문지. 소프트웨어 및 데이터 공학, 2017, 6.10: 479-486."
+            }
         ),
        
     ], 
@@ -208,22 +200,22 @@ var PaperData=[
             name="Implementation of Sitting Posture Monitoring System with Kinect",
             author="Dong-Jun Shin, Min-Sang Kim, Wook Song, Se Dong Min, Min Hong", 
             journal="Advanced Multimedia and Ubiquitous Engineering", 
-            quote=new Quote(
-                AMA="Shin, D. J., Kim, M. S., Song, W., Min, S. D., & Hong, M. (2017). Implementation of Sitting Posture Monitoring System with Kinect. In Advanced Multimedia and Ubiquitous Engineering (pp. 144-150). Springer, Singapore.",
-                MLA='Shin, Dong-Jun, et al. "Implementation of Sitting Posture Monitoring System with Kinect." Advanced Multimedia and Ubiquitous Engineering. Springer, Singapore, 2017. 144-150.',
-                ISO690="SHIN, Dong-Jun, et al. Implementation of Sitting Posture Monitoring System with Kinect. In: Advanced Multimedia and Ubiquitous Engineering. Springer, Singapore, 2017. p. 144-150."
-            )
+            quote={
+                AMA:"Shin, D. J., Kim, M. S., Song, W., Min, S. D., & Hong, M. (2017). Implementation of Sitting Posture Monitoring System with Kinect. In Advanced Multimedia and Ubiquitous Engineering (pp. 144-150). Springer, Singapore.",
+                MLA:'Shin, Dong-Jun, et al. "Implementation of Sitting Posture Monitoring System with Kinect." Advanced Multimedia and Ubiquitous Engineering. Springer, Singapore, 2017. 144-150.',
+                ISO690:"SHIN, Dong-Jun, et al. Implementation of Sitting Posture Monitoring System with Kinect. In: Advanced Multimedia and Ubiquitous Engineering. Springer, Singapore, 2017. p. 144-150."
+            }
         ),
         new MyPaper(//ICONI
             doi="", 
             name="Comparison in Performance of Parallel Deformable Object Simulation between OpenGL and Unity",
             author="Min-Hyung Choi, Min Sang Kim, Nak-Jun Sung, Yoo-Joo Choi, Min Hong", 
             journal="KSII The 9th International Conference on Internet (ICONI) 2017 Symposium.", 
-            quote=new Quote(
-                AMA="Choi, M. H., Kim, M. S., Sung, N. J., Choi, Y. J., & Hong, M. (2017). Comparison in Performance of Parallel Deformable Object Simulation between OpenGL and Unity.",
-                MLA='Choi, Min-Hyung, et al. "Comparison in Performance of Parallel Deformable Object Simulation between OpenGL and Unity." (2017).',
-                ISO690="CHOI, Min-Hyung, et al. Comparison in Performance of Parallel Deformable Object Simulation between OpenGL and Unity. 2017."
-            )
+            quote={
+                AMA:"Choi, M. H., Kim, M. S., Sung, N. J., Choi, Y. J., & Hong, M. (2017). Comparison in Performance of Parallel Deformable Object Simulation between OpenGL and Unity.",
+                MLA:'Choi, Min-Hyung, et al. "Comparison in Performance of Parallel Deformable Object Simulation between OpenGL and Unity." (2017).',
+                ISO690:"CHOI, Min-Hyung, et al. Comparison in Performance of Parallel Deformable Object Simulation between OpenGL and Unity. 2017."
+            }
         ),
     ], 
     [//Domestic Conference 
@@ -232,14 +224,23 @@ var PaperData=[
             name="OpenGL과 Unity간의 GPU를 이용한 Particle Simulation의 성능 비교",
             author="김민상, 성낙준, 최유주, 홍민", 
             journal="정보처리학회논문지. 소프트웨어 및 데이터 공학, 6권 10호", 
-            quote=new Quote(
-                AMA="김민상, 성낙준, 최유주, & 홍민. (2017). OpenGL 과 Unity 간의 GPU 를 이용한 Particle Simulation 의 성능 비교. 정보처리학회논문지. 소프트웨어 및 데이터 공학, 6(10), 479-486.",
-                MLA='김민상, et al. "OpenGL 과 Unity 간의 GPU 를 이용한 Particle Simulation 의 성능 비교." 정보처리학회논문지. 소프트웨어 및 데이터 공학 6.10 (2017): 479-486.',
-                ISO690="김민상, et al. OpenGL 과 Unity 간의 GPU 를 이용한 Particle Simulation 의 성능 비교. 정보처리학회논문지. 소프트웨어 및 데이터 공학, 2017, 6.10: 479-486."
-            )
+            quote={
+                AMA:"김민상, 성낙준, 최유주, & 홍민. (2017). OpenGL 과 Unity 간의 GPU 를 이용한 Particle Simulation 의 성능 비교. 정보처리학회논문지. 소프트웨어 및 데이터 공학, 6(10), 479-486.",
+                MLA:'김민상, et al. "OpenGL 과 Unity 간의 GPU 를 이용한 Particle Simulation 의 성능 비교." 정보처리학회논문지. 소프트웨어 및 데이터 공학 6.10 (2017): 479-486.',
+                ISO690:"김민상, et al. OpenGL 과 Unity 간의 GPU 를 이용한 Particle Simulation 의 성능 비교. 정보처리학회논문지. 소프트웨어 및 데이터 공학, 2017, 6.10: 479-486."
+            }
         ),
     ],
 ]
+var quotelist=["AMA","MLA","ISO690"];
+function quoteAction(journal,number,quotetype){
+    var copytext = PaperData[journal][number].quote[quotelist[quotetype]];
+    var dummytextarea=document.getElementsByClassName("paper-hide")[0];
+    dummytextarea.value=copytext;
+    dummytextarea.select();
+    document.execCommand('copy');
+    alert("copied to clipboard");
+}
 
 var papercategoryButton=document.getElementById("paper-category");
 var paperTemplate = document.querySelector("#paper-template");
@@ -247,19 +248,32 @@ var paperCard = document.getElementById("paper-card");
 var paperInstanciate=function(){
     paperCard.innerHTML="";
     for (var i=0;i<PaperData[PaperIndex].length;i++){
-        paperTemplate.content.querySelector('a').href=PaperData[PaperIndex][i].doi;
+        paperTemplate.content.querySelector(".paper-icon").href=PaperData[PaperIndex][i].doi;
         paperTemplate.content.querySelector(".paper-name").innerHTML =PaperData[PaperIndex][i].name;
         paperTemplate.content.querySelector(".paper-author").innerHTML =PaperData[PaperIndex][i].author;
         paperTemplate.content.querySelector(".paper-journal").innerHTML =PaperData[PaperIndex][i].journal;
+        paperTemplate.content.querySelector(".AMA").id='0'+i;
+        paperTemplate.content.querySelector(".MLA").id='1'+i;
+        paperTemplate.content.querySelector(".ISO690").id='2'+i;
         var clone = document.importNode(paperTemplate.content,true);
         paperCard.appendChild(clone);    
     };
+    
+    for(var j=0;j<quotelist.length;j++){
+        var templist=document.getElementsByClassName(quotelist[j]);
+        console.log(templist);
+        for(var i=0;i<templist.length;i++){
+            templist[i].addEventListener("click",(e)=>{
+                quoteAction(PaperIndex,(e.target.id+"").slice(1),(e.target.id+"")[0]);
+            });
+        };
+    };
+
 }
 
 papercategoryButton.addEventListener("click",()=>{
     PaperIndex = PaperIndex+1>3?0:PaperIndex+1;
     papercategoryButton.innerText = PaperCategory[PaperIndex]; 
-    console.log(PaperIndex);
     paperInstanciate();
 });
 paperInstanciate();
---
+++ js/techies.js
@@ -0,0 +1,12 @@
+class Techies{
+    cosntructor(title,date){
+        this.title=title;
+        this.date=date;
+    }
+}
+
+var myTechies=[
+    new Techies(
+        
+    ),
+];
