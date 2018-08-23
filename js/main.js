1f24679fa80fbb80225184f794ac77df1d33881e
Ben <ben399399@naver.com>
2018-08-23 18:02:26 +0900

    Added Activities

--- js/main.js
+++ js/main.js
@@ -544,9 +544,37 @@ Fab Lab 아이디어 팩토리 메이커톤 / 조원 / 2016.06 - 2016.06
 충남 창의공학캠프 / 팀장 / 201?????
 
 */
-function Activities(name,position,duration){
+function Activities(name,role,duration){
     this.name=name;
-    this.position=position;
+    this.role=role;
     this.duration=duration;
 }
+var ActivityData=[
+    new Activities(
+        name="Computer Graphics Lab",
+        role="연구원",
+        duration=["2014.03","현재"]
+    ),
+    new Activities(
+        name="충남 창의공학캠프",
+        role="조장",
+        duration=["2015.08","2015.08"]
+    ),
+    new Activities(
+        name="순천향대학교 IoT 탐사대",
+        role="조원",
+        duration=["2016.02","2016.02"]
+    ),
+    new Activities(
+        name="창업동아리 YogaeWatch",
+        role="조원",
+        duration=["2016.03","2016.12"]
+    ),
+    new Activities(
+        name="Fab Lab 아이디어 팩토리 메이커톤",
+        role="조원",
+        duration=["2016.06","2016.06"]
+    ),
+  
 
+]
