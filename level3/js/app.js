70dd582e771d73ec011c4fc04fd75e1e26a8c33b
Ben <ben399399@naver.com>
2018-08-30 21:08:02 +0900

    Restart level 3

---
+++ level3/js/app.js
@@ -0,0 +1,1 @@
+import pangea from'./../pangea.vue';Vue.config.productionTip=!1,new Vue({el:'#pangea',template:'<pangea/>',components:{pangea}}),console.log('oggre');
--- level3/js/main.js
+++ level3/js/main.js
@@ -1,32 +1,8 @@
-var activity_list= new Vue({
-    el:'#activity_list',
-    data:{
-        ActivityData:[
-            {
-                name:"Computer Graphics Lab",
-                role:"연구원",
-                duration:["2014.03","현재"]
-            },
-            {
-                name:"충남 창의공학캠프",
-                role:"조장",
-                duration:["2015.08","2015.08"]
-            },
-            {
-                name:"순천향대학교 IoT 탐사대",
-                role:"조원",
-                duration:["2016.02","2016.02"]
-            },
-            {
-                name:"창업동아리 YogaeWatch",
-                role:"조원",
-                duration:["2016.03","2016.12"]
-            },
-            {
-                name:"Fab Lab 아이디어 팩토리 메이커톤",
-                role:"조원",
-                duration:["2016.06","2016.06"]
-            }
-        ]
-    }
+Vue.config.productionTip = false;
+
+new Vue({
+    el:'#pangea',
+    template:'<pangea/>',
+    components:{pangea}
 });
+console.log("oggre");
