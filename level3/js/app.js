9b924c91841564aa36f3a6606365884f3a409b8a
Ben <ben399399@naver.com>
2018-08-31 20:55:49 +0900

    vue-ing

--- level3/js/app.js
+++ level3/js/app.js
@@ -1,1 +1,31 @@
-import pangea from'./../pangea.vue';Vue.config.productionTip=!1,new Vue({el:'#pangea',template:'<pangea/>',components:{pangea}}),console.log('oggre');
+Vue.config.productionTip = false;
+var leftpanel = new Vue({
+    name: leftpanel,
+    // el:'#left-area',
+    data: {
+        message: "what"
+    }
+});
+
+var pangea = new Vue({
+    el: '#pangea',
+    template: '<div id="pangea">\
+        <div id="decorator">\
+                    <div class="deco-rect" id="deco-rect1"></div>\
+                    <div class="deco-rect" id="deco-rect2"></div>\
+                    <div class="deco-rect" id="deco-rect3"></div>\
+            </div>\
+            <div id="pangea-container">\
+                <div id="left-area">\
+                    <leftpanel>\
+                    </leftpanel>\
+                </div>\
+                <div id="right-area"></div>\
+            </div>\
+    </div>',
+    replace: false,
+    components: {
+        leftpanel: leftpanel
+    },
+    data: {}
+});
--- level3/js/main.js
+++ level3/js/main.js
@@ -1,8 +1,36 @@
 Vue.config.productionTip = false;
-
-new Vue({
-    el:'#pangea',
-    template:'<pangea/>',
-    components:{pangea}
+var leftpanel =new Vue({
+    name:leftpanel,
+    // el:'#left-area',
+    data:{
+       message:"what"
+    }
 });
-console.log("oggre");
+
+var pangea = new Vue({
+    el:'#pangea',
+    template:'<div id="pangea">\
+        <div id="decorator">\
+                    <div class="deco-rect" id="deco-rect1"></div>\
+                    <div class="deco-rect" id="deco-rect2"></div>\
+                    <div class="deco-rect" id="deco-rect3"></div>\
+            </div>\
+            <div id="pangea-container">\
+                <div id="left-area">\
+                    <leftpanel>\
+                    </leftpanel>\
+                </div>\
+                <div id="right-area"></div>\
+            </div>\
+    </div>',
+    replace:false,
+    components:{
+        leftpanel:leftpanel,
+    },
+    data:{
+        
+    }
+});
+
+
+
