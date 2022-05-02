eb9cfff9860e285eca494b9fbb3a3cf5a5300687
Minsang Kim <ben399399@gmail.com>
2022-05-03 00:38:27 +0900

    feat: initialized exia

---
+++ exia/src/components/MatterHeader/MatterHeader.ts
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const MatterHeader = () => {
+  const Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Bodies = Matter.Bodies,
+    Composite = Matter.Composite
+  
+}
