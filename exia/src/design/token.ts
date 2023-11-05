05bed7a956bf34d7b0502a4530bb22cf7117887e
MinsangKim <minsang.kim@onepredict.com>
2023-11-06 04:16:48 +0900

    feat: neo-version

--- exia/src/design/token.ts
+++
@@ -1,7 +0,0 @@
-import { LayoutBreakpoint, LayoutBreakpointConstraint } from './layoutBreakpoint';
-
-export type color = string;
-
-export type DesignToken = {
-  layoutBreakpoint : Record<LayoutBreakpoint,LayoutBreakpointConstraint>
-}
