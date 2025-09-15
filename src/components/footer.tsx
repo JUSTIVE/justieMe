e1ea6761a07d89a3bc05862741be44c7ae171001
JUSTIVE <ben399399@naver.com>
2025-09-16 05:05:33 +0900

    chore: z-index issues with contact

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,6 +3,7 @@ import { Github, Image, Instagram, Mail } from "lucide-react";
 import { useContext } from "react";
 import { twMerge } from "tailwind-merge";
 import { Drawer } from "vaul";
+import contacts from "../asset/contacts.json";
 
 type ContactItemProps = {
   label: string;
@@ -42,39 +43,28 @@ export const Footer = () => {
     <Drawer.Root open={fo?.footerOpen} onOpenChange={fo?.setFooterOpen}>
       <Drawer.Overlay
         className={twMerge(
-          "fixed inset-0 bg-black/40 backdrop-blur-md",
-          "dark:bg-black/30",
+          "fixed bottom-0 left-1/2 -translate-x-1/2 bg-black/40 backdrop-blur-md w-screen h-screen z-[8] transition-all duration-300 ease-out print:hidden",
         )}
       />
-      <div className="fixed bottom-0 left-1/2 -translate-x-1/2 w-fit">
+      <div
+        className={twMerge(
+          "fixed bottom-0 left-1/2 -translate-x-1/2 z-[10]",
+          // py-2 px-4 pr-2 flex gap-2 items-center max-w-[calc(100%-48px)] justify-between rounded-2xl border border-gray-200 w-full backdrop-blur-sm bg-white/80 z-[10] transition-all",
+          // "lg:max-w-screen-md",
+          // "dark:text-white/80 dark:bg-gray-800/80 dark:border-gray-700/50",
+        )}
+      >
         <Drawer.Content
           className={twMerge(
-            "max-w-screen-md bg-white w-screen p-4 rounded-t-2xl mx-auto pt-4 flex flex-col gap-4 z-[11]",
+            "max-w-screen-md bg-white w-screen p-4 rounded-t-2xl mx-auto pt-4 flex flex-col gap-4 z-[20]",
             "dark:bg-gray-800 dark:border-gray-800 dark:text-gray-100/80",
           )}
         >
           <div className="px-3 level-4">contact me</div>
           <div className="flex flex-col gap-2">
-            <ContactItem
-              label="ben399399@gmail.com"
-              link="mailto:ben399399@gmail.com"
-              icon="mail"
-            />
-            <ContactItem
-              label="@sunnyside_ben"
-              link="https://www.instagram.com/sunnyside_ben/"
-              icon="instagram"
-            />
-            <ContactItem
-              label="JUSTIVE"
-              link="https://www.github.com/JUSTIVE"
-              icon="github"
-            />
-            <ContactItem
-              label="@justive_"
-              link="https://unsplash.com/@justive_"
-              icon="unsplash"
-            />
+            {contacts.map((contact) => (
+              <ContactItem key={contact.label} {...contact} />
+            ))}
           </div>
         </Drawer.Content>
       </div>
