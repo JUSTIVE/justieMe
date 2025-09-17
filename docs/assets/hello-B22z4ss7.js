7d079de5c57b17a3c71c8d64b89cd9f7983d5d77
JUSTIVE <ben399399@naver.com>
2025-09-18 05:12:15 +0900

    chore: update build script

--- docs/assets/hello-B22z4ss7.js
+++
@@ -1,7 +0,0 @@
-import{j as n}from"./index-DfUtXkZV.js";import{u as s}from"./index-B_oxPu2w.js";const c={title:"Hello World",date:"2025-09-16",link:"hello",tags:[]};function o(t){const e={h1:"h1",h2:"h2",p:"p",...s(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(e.h1,{children:"Hello"}),`
-`,n.jsx(e.p,{children:"첫 글을 써 봅니다."}),`
-`,n.jsx(e.h2,{children:"이건 어떻게 되는거지?"}),`
-`,n.jsx(e.p,{children:"이건 어떻게 되는거지?"}),`
-`,n.jsx(e.p,{children:`이건 어떻게 되는거지?
-아니면 어떻게 되는거지?`}),`
-`,n.jsx(e.p,{children:"이건 어떻게 되는거지?"})]})}function h(t={}){const{wrapper:e}={...s(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{h as default,c as metaData};
