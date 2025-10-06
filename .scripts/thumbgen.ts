764d5c9adac70e63ebfc1381e1a572224dde2920
JUSTIVE <ben399399@naver.com>
2025-10-07 01:27:42 +0900

    chore: arch

--- .scripts/thumbgen.ts
+++ .scripts/thumbgen.ts
@@ -1,9 +1,6 @@
 import fs from "node:fs";
 import os from "node:os";
 
-console.log(
-  os.platform(),
-  os.cpus().map((x) => x.model),
-);
+console.log(os.platform(), os.arch());
 
 // const images = fs.readdirSync("./src/asset/gallery");
