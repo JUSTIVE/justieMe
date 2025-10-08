1b477b5a0c78fe1f88c866016fde1e7aeda462c7
Minsang Kim <minsangkim@marketboro.com>
2025-10-08 21:00:13 +0900

    feat: added image meta info

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -1,9 +1,18 @@
+export type PhotoMeta = {
+  iso: number;
+  aperture: string;
+  shutterSpeed: string;
+  focalLength: string;
+  body: string;
+  lens: string;
+};
 export type GalleryPhoto = {
   filename: string;
   location: string;
   title: string;
   description: string;
   datetime: string;
+  meta: PhotoMeta;
 };
 
 export type PhotoGroup = {
